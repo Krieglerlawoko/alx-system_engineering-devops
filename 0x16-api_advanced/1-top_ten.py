6bd83ed16fd105896e884be1f6580f7df2b0cbd8
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-04-11 09:40:46 -0700

    n

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""
-Hot posts printed on a given Reddit subreddit.
-"""
-
+'''
+    function top_ten
+'''
 import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    """Titles of 10 hottest posts printed on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-
+    '''
+        top ten posts for a given subreddit returned
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise exception for HTTP errors
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        else:
-            print("None")
-    except requests.HTTPError as e:
-        if e.response.status_code == 404:
-            print("None")
-        else:
-            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    top_ten(argv[1])
---
+++ 0x19-postmortem/README.md
@@ -0,0 +1,1 @@
+alx 0x19-postmortem read me file
