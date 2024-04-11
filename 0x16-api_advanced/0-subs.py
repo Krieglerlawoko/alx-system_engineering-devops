4abc16c243ec5e5ec167d224e928376db66ec651
Kriegler lawoko <Krieglerlawokojudge@gmail.com>
2024-04-11 07:41:26 -0700

    new

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""
-Subscribers queried on a given Reddit subreddit.
-"""
-
+'''
+    function number_of_subscribers
+'''
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """Total number of subscribers returned on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    '''
+        Number of subscribers for given subreddit returned
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,50 +1,31 @@
 #!/usr/bin/python3
-"""
-List of all hot posts querried on a given Reddit subreddit.
-"""
-
+"""module Contains recurse function"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """
-    List of titles of all hot posts
-    recursively retrieved on a given subreddit.
-    """
-    # URL for the subreddit's hot posts constructed in JSON format
+    """List of titles of all hot posts on a given subreddit returned."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    # headers for the HTTP request defined, including User-Agent
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
-
-    # parameters for the request defined, including pagination and limit
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-
-    # GET request sent to the subreddit's hot posts page
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    # Check if the response status code indicates a not-found error (404)
     if response.status_code == 404:
         return None
-    # Parse the JSON response and extract relevant data
-    result = response.json().get("data")
-    after = result.get("after")
-    count += result.get("dist")
 
-    # Append post titles to the hot_list
-    for a in result.get("children"):
+    res = response.json().get("data")
+    after = res.get("after")
+    count += res.get("dist")
+    for a in res.get("children"):
         hot_list.append(a.get("data").get("title"))
 
-    # If there are more posts to retrieve, recursively call the function
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
-
-    # Return the final list of hot post titles
     return hot_list
