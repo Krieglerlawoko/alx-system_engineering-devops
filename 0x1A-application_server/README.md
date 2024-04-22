30b08f3d720d11d7cb1ad7989ce4f1998b3ac1f0
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-04-22 07:00:49 -0700

    n

---
+++ 0x1A-application_server/README.md
@@ -0,0 +1,1 @@
+alx 0x1A-application_server read me file
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
 S configuration changed so that it is possible to login with the
 
-exec {'OS security config':
+{'OS security config':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
   path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
-
